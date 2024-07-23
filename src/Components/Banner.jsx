0703429631cb09bb20e118585a83a0d857ba7c3c
hult21 <58756622+hult21@users.noreply.github.com>
2024-07-23 13:45:25 +0200

    Fix links to include basename

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import sale from "../Images/100a1728-ade0-4a48-8357-4394a6e69065.jpg";
 import skincare from "../Images/a4bb50c701b569fddfabffea35892e45.jpg";
-import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
 
@@ -12,14 +11,14 @@ const Banner = () => {
         <img src={sale} alt="Complexion"/>
         <div className="overlay">
           <h2>COMPLEXION</h2>
-          <a href="/e-commerce-website/shop/complexion">Discover</a>
+          <a href="/shop/complexion">Discover</a>
         </div>
       </div>
       <div className="product">
         <img src={skincare} alt="Skincare"/>
         <div className="overlay">
           <h2>SKINCARE</h2>
-          <a href="/e-commerce-website/shop/skincare">Discover</a>
+          <a href="/shop/skincare">Discover</a>
         </div>
       </div>
     </div>
